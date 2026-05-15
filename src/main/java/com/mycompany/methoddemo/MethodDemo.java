68f81b800d9b574fe8a3bc146604740179159b75
Uyanda <uyandatracy@gmail.com>
2026-05-15 11:49:41 +0200

    Updated MethodDemo with sum method

--- src/main/java/com/mycompany/methoddemo/MethodDemo.java
+++ src/main/java/com/mycompany/methoddemo/MethodDemo.java
@@ -1,7 +1,15 @@
 package com.mycompany.methoddemo;
 
 public class MethodDemo {
+    
     public static void main(String[] args) {
-        System.out.println("Sum from 1 to 5 is " + (1+2+3+4+5));
+        System.out.println("Sum from 1 to 5 is " + sum(1, 5)+ " using a method");
+    }
+    
+    public static int sum(int i1, int i2) {
+        int result = 0;
+        for (int i = i1; i <= i2; i++)
+            result += i;
+        return result;
     }
 }
