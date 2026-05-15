4753c1e51d4f032647dce41eeee0521d46ab7afb
Uyanda <uyandatracy@gmail.com>
2026-05-15 11:11:47 +0200

    Initial commit - MethodDemo class

---
+++ src/main/java/com/mycompany/methoddemo/MethodDemo.java
@@ -0,0 +1,7 @@
+package com.mycompany.methoddemo;
+
+public class MethodDemo {
+    public static void main(String[] args) {
+        System.out.println("Sum from 1 to 5 is " + (1+2+3+4+5));
+    }
+}
